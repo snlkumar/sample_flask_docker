f3d5f06a5ffc5642e0fa9eff6a92a1a31c7989d5
riteshkranjan <riteshk.ranjan@gmail.com>
2020-11-12 12:10:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
   
 @app.route('/') 
 def hello(): 
-    return "welcome to the flask tutorials wrong"
+    return "welcome to the flask tutorials"
   
   
 if __name__ == "__main__": 
